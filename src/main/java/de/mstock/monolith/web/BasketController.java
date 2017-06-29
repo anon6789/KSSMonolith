7d47284c11168255824a615182f08daef44d5b4e
jelto <anon6789@jel.to>
2017-06-29 17:54:21 +0200

    added total sum to basket

--- src/main/java/de/mstock/monolith/web/BasketController.java
+++ src/main/java/de/mstock/monolith/web/BasketController.java
@@ -7,21 +7,19 @@ import java.util.Locale;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.client.RestTemplate;
 
 import de.mstock.monolith.domain.Basket;
 import de.mstock.monolith.domain.BasketEntryDto;
-import de.mstock.monolith.domain.Product;
-import de.mstock.monolith.domain.ProductList;
 import de.mstock.monolith.service.ShopService;
 
 @Controller
 public class BasketController {
 
   private static final String TEMPLATE = "basket";
+  private double totalPrice = 0;
 
   @Autowired
   private ShopService shopService;
@@ -36,16 +34,11 @@ public class BasketController {
    */
   @RequestMapping(value = "/basket", method = RequestMethod.GET)
   public String category(Model model, Locale locale) {
-	  
-	List<ProductDTO> testBasket = shopService.getProductsForCategory(locale, "vegetables");
-	
+	  	
 	Basket basket = consumeRESTService();
-	  
-    //model.addAttribute("categories", shopService.getCategories(locale));
-    //model.addAttribute("products", shopService.getProductsForCategory(locale, prettyUrlFragment));
-    //model.addAttribute("prettyUrlFragment", prettyUrlFragment);
-	//model.addAttribute("basket", testBasket);
+
 	model.addAttribute("basket", basketToDTO(basket, locale));
+	model.addAttribute("totalPrice", totalPrice);
     return TEMPLATE;
   }
   
@@ -57,13 +50,22 @@ public class BasketController {
   
   private List<ProductDTO> basketToDTO(Basket basket, Locale locale){
 	  ArrayList<ProductDTO> products = new ArrayList<ProductDTO>();
+	  totalPrice = 0;
 	  
 	  for (BasketEntryDto product : basket.getProductList()){
 		  ProductDTO proDTO = shopService.getProduct(locale, product.getProductDto().getProductId());
 		  proDTO.setCount(product.getCount().toString());
 		  products.add(proDTO);
+		  //TODO: Implement this calculation in frontend for better code-consistency 
+		  totalPrice += calcPrice(proDTO.getPrice(), proDTO.getCount());
 	  }
-	  
 	  return products;
   }
+  
+  private double calcPrice(String price, String count){
+	  double articlePrice = 0;
+	  articlePrice = Double.valueOf(count) * Double.valueOf(price);  
+	  return articlePrice;
+  }
+  
 }
